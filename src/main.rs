b58564c8cbdfce2adb5142718de5c4a2247ca87e
markkovari <kovarimarkofficial@gmail.com>
2020-12-01 03:55:00 +0100

    yeah cheating

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,39 @@
 extern crate cronjob;
 use cronjob::CronJob;
+use std::env::set_current_dir;
 use std::fs;
 use std::process::Command;
 
 fn main() {
     let mut cron = CronJob::new("xmas_tree", on_cron);
     cron.seconds("0");
-    cron.minutes("0");
-    cron.hours("8-16");
+    cron.minutes("0-59");
+    // cron.hours("8-16");
     cron.start_job();
 }
 
 fn on_cron(_name: &str) {
+    println!("READIND FILE");
+    let _file_dir = set_current_dir("/home/mark/DEV/xmasTree").unwrap();
     let mut contents = fs::read_to_string("./README.md")
         .expect("Something went wrong reading the file, maybe you should do it the lame way");
     if contents.len() % 2 == 0 {
         let _result = fs::write("./README.md", format!("{} ", contents));
-        if _result.is_ok() {
-            println!("updated with a newchar")
-        }
     } else {
         contents.remove(contents.len() - 1);
         let _result = fs::write("./README.md", contents);
-        if _result.is_ok() {
-            println!("updated with a removed char")
-        }
     }
 
     let mut _git_add_result = Command::new("git").arg("add").arg("-A").output().unwrap();
-    println!("{:?}", _git_add_result);
     let mut _git_commit_result = Command::new("git")
         .arg("commit")
         .arg("-m")
         .arg("yeah cheating")
         .output()
         .unwrap();
-        println!("{:?}", _git_commit_result);
-
     let mut _git_push_result = Command::new("git").arg("push").output().unwrap();
-    println!("{:?}", _git_push_result);
-
+    println!(
+        "achtung-achtung {:?} {:?} {:?}",
+        _git_add_result, _git_commit_result, _git_push_result
+    );
 }
